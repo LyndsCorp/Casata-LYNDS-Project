12cccec7704cee634bcb3623d77f5048ecb26c1c
Monojo Project <monojo.lynds@gmail.com>
2026-07-04 22:17:47 +0200

    Update lyndsfetch.sh

--- lyndsfetch.sh
+++ lyndsfetch.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Desarrollado por David Baña Szymaniak. Licencia GPL v3, LYNDS Project
-# Version 1.2.3
+# Version 1.2.4
 
 # Ruta del archivo de configuración
 CONFIG_FILE="$HOME/.config/LyndsFetch/config.json"
