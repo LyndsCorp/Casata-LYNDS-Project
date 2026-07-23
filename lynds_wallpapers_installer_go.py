c9aab1207ec5b05bb663c9155fc306925dab24a8
David Baña Szymaniak <monojo.lynds@gmail.com>
2026-07-23 16:28:28 +0200

    Actualizo las rutas a los iconos para que se integren correctamente con KDE Plasma 6, aunque este sea para GNOME

--- lynds_wallpapers_installer_go.py
+++ lynds_wallpapers_installer_go.py
@@ -39,7 +39,7 @@ class LyndsExecutive(tk.Tk):
         self.configure(bg="#0a0c0a")
 
         try:
-            icon_img = tk.PhotoImage(file="/usr/share/icons/LyndsOS/lynds-wallpapers-go.png")
+            icon_img = tk.PhotoImage(file="/usr/share/icons/hicolor/256x256/apps/lynds-wallpapers-go.png")
             self.iconphoto(False, icon_img)
         except Exception:
             pass
